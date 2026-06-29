8a8f62b6774ff33e53ede47881fc7f98f16789cb
omr8n <omranaboaliii@gmail.com>
2026-06-30 00:55:31 +0300

    befffor_notfication

--- lib/features/admin/domain/repos/app_config_repo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:test_graduation/core/errors/failures.dart';
-import 'package:test_graduation/core/models/app_config_model.dart';
-
-abstract class AppConfigRepo {
-  Future<Either<Failure, AppConfigModel>> getConfig();
-  Future<Either<Failure, void>> updateConfig(AppConfigModel config);
-}
